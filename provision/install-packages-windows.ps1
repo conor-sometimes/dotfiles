9a84b255acf2d7b92a264a27ff64b08a7c4c179a
Conor McShane <conor.d.mcshane@gmail.com>
2023-04-01 02:53:55 +0100

    chatGPT is a dork
    Signed-off-by: Conor McShane <conor.d.mcshane@gmail.com>

--- provision/install-packages-windows.ps1
+++ provision/install-packages-windows.ps1
@@ -1,14 +1,12 @@
 #Requires -RunAsAdministrator
 
-# Install winget
+# Install Microsoft Store App Installer
 $packageName = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"
-
 if (!(Get-AppxPackage $packageName -ErrorAction SilentlyContinue)) {
-    # Install winget
-    Write-Output "Installing $packageName"
     Add-AppxPackage -RegisterByFamilyName -MainPackage $packageName
 }
 
+# Install packages
 $PACKAGES = @(
     'Anki.Anki'
     'AntibodySoftware.WizTree'
@@ -17,7 +15,7 @@ $PACKAGES = @(
     'Cryptomator.Cryptomator'
     'Doist.Todoist'
     'GIMP.GIMP'
-    'Git.Git',
+    'Git.Git'
     'KeePassXCTeam.KeePassXC'
     'Klocman.BulkCrapUninstaller'
     'Microsoft.PowerShell'
@@ -41,33 +39,24 @@ if ($env:Username -eq "Lewis") {
     $PACKAGES += 'BlenderFoundation.Blender'
 }
 
-Write-Output "Installing packages..."
-$progress = 0
-$PACKAGES.Count | ForEach-Object {
-    $PACKAGE = $PACKAGES[$_]
-    $progress++
-    Write-Progress -Activity "Installing package $PACKAGE" -Status "Progress: $progress of $($PACKAGES.Count)" -PercentComplete ($progress / $PACKAGES.Count * 100)
+Foreach ($PACKAGE in $PACKAGES) {
+    Write-Host "Installing $PACKAGE ..."
     winget install -e --id $PACKAGE
 }
 
+# Install Visual Studio Code extensions
 $EXTENSIONS = @(
     'eamodio.gitlens'
 )
 
-Write-Output "Installing extensions..."
-$progress = 0
-$EXTENSIONS.Count | ForEach-Object {
-    $EXTENSION = $EXTENSIONS[$_]
-    $progress++
-    Write-Progress -Activity "Installing extension $EXTENSION" -Status "Progress: $progress of $($EXTENSIONS.Count)" -PercentComplete ($progress / $EXTENSIONS.Count * 100)
+Foreach ($EXTENSION in $EXTENSIONS) {
     code --install-extension $EXTENSION
 }
 
-# Check if WSL is already installed
+# Install WSL
 if ($env:Username -eq "dind" -Or $env:Username -eq "Conor") {
     $wslInstalled = wsl.exe --list --all | Select-String -Pattern "Ubuntu"
     if (!$wslInstalled) {
-        Write-Output "Installing WSL..."
         wsl --install
     } else {
         Write-Output "WSL is already installed"
