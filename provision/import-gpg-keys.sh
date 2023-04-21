add9a180505b223a6e8956960451342d9d847d4a
Conor McShane <conor.d.mcshane@gmail.com>
2023-04-21 23:23:45 +0100

    Updated provision script
    Signed-off-by: Conor McShane <conor.d.mcshane@gmail.com>

--- provision/import-gpg-keys.sh
+++ provision/import-gpg-keys.sh
@@ -8,6 +8,7 @@
 
 set -o errexit
 set -o nounset
+set -o pipefail
 
 readonly HOST_DESKTOP="honshu"
 readonly HOST_SERVER="hokkaido"
@@ -16,34 +17,51 @@ readonly HOST_MACBOOK="PY60HY3QW3"
 readonly REPO_DIR="$HOME/.local/share/chezmoi"
 
 KEY=""
-TRUST=""
+GPG_OWNER_TRUST=""
 
-case "$(hostname)" in
+add_ssh_key() {
+  public_key="$1"
+  if ! grep -qxF "$public_key" ~/.ssh/authorized_keys; then
+    echo "Adding public key to authorized_keys file"
+    echo "$public_key" >> ~/.ssh/authorized_keys
+  fi
+}
+
+add_ssh_keys() {
+  for key_file in "$@"; do
+    echo "adding $key_file"
+    public_key=$(< "$key_file")
+    if ! grep -qxF "$public_key" ~/.ssh/authorized_keys; then
+      echo "Adding public key from $key_file to authorized_keys file"
+      echo "$public_key" >> ~/.ssh/authorized_keys
+    fi
+  done
+}
+
+case "$HOSTNAME" in
   $HOST_MACBOOK)
     echo "Macbook"
     KEY="macbook"
-    TRUST="C59E2EA148098193BEEEE42087BFE2E84FE74F61:6:"
-    gpg --import "$REPO_DIR/provision/gpg/public/yubikey-public-key.asc"
-    gpg --import "$REPO_DIR/provision/gpg/public/server-public-key.asc"
-    gpg --import "$REPO_DIR/provision/gpg/public/desktop-public-key.asc"
+    GPG_OWNER_TRUST="C59E2EA148098193BEEEE42087BFE2E84FE74F61:6:"
+    gpg --import "$REPO_DIR/provision/gpg/public/yubikey-public-key.asc" "$REPO_DIR/provision/gpg/public/server-public-key.asc"
     ;;
+
   $HOST_SERVER)
     echo "Server"
     KEY="server"
-    TRUST="7032F7F9BC73C52B99956EB1F79E4AE90F072A31:6:"
-    gpg --import "$REPO_DIR/provision/gpg/public/yubikey-public-key.asc"
-    gpg --import "$REPO_DIR/provision/gpg/public/macbook-public-key.asc"
-    gpg --import "$REPO_DIR/provision/gpg/public/desktop-public-key.asc"
+    GPG_OWNER_TRUST="7032F7F9BC73C52B99956EB1F79E4AE90F072A31:6:"
+    gpg --import "$REPO_DIR/provision/gpg/public/yubikey-public-key.asc" "$REPO_DIR/provision/gpg/public/macbook-public-key.asc"
+
+    echo "adding ssh key"
+    add_ssh_keys "$REPO_DIR/dot_ssh/macbook.pub"
     ;;
+
   $HOST_DESKTOP)
     echo "Desktop"
     KEY="desktop"
-    gpg --import "$REPO_DIR/provision/gpg/public/yubikey-public-key.asc"
-    gpg --import "$REPO_DIR/provision/gpg/public/macbook-public-key.asc"
-    gpg --import "$REPO_DIR/provision/gpg/public/server-public-key.asc"
+    gpg --import "$REPO_DIR/provision/gpg/public/yubikey-public-key.asc" "$REPO_DIR/provision/gpg/public/server-public-key.asc"
     ;;
 esac
 
-
 gpg --decrypt "$REPO_DIR/provision/gpg/private/${KEY}-private-key.gpg.asc" | gpg --import
-echo "$TRUST" | gpg --import-ownertrust
+echo "$GPG_OWNER_TRUST" | gpg --import-ownertrust
