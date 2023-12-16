640c51d30ef006a243494e0ddf864b8e4eee8e7d
Conor McShane <conor.d.mcshane@outlook.com>
2023-12-16 02:26:28 +0000

    Small nixos config changes
    Signed-off-by: Conor McShane <conor.d.mcshane@outlook.com>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,15 +54,15 @@
   hardware.opengl = {
     enable = true;
     driSupport = true;
-    #driSupport32Bit = true;
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
+  # services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;
     nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   users.users.conor= {
