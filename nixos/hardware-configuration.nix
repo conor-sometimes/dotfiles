bd33e0f46a2588f03fc9feb26f7a90be47ecc4a1
Conor McShane <conor.mcshane@yahooinc.com>
2024-01-10 22:06:18 +0000

    Clean up nixos config

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,28 +4,32 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports =[ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+      kernelModules = [ "dm-snapshot" ];
+    };
+  };
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4784ad81-dc8a-4422-879c-f45c838c4320";
+    {
+      device = "/dev/disk/by-uuid/4784ad81-dc8a-4422-879c-f45c838c4320";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5258-192D";
+    {
+      device = "/dev/disk/by-uuid/5258-192D";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/2f3e1cd4-3df6-4328-b4fa-8b42b46fd5e0"; }
-    ];
+    [ { device = "/dev/disk/by-uuid/2f3e1cd4-3df6-4328-b4fa-8b42b46fd5e0"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
